625620d1697dc3a9e46b6b458fd04b3ea2bf1da7
Sergey_Ivanov <ivanovsergey.samara@gmail.com>
2023-02-10 15:37:41 +0400

    Initial commit

---
+++ src/test/java/com/ivanov_sergey/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanov_sergey.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
